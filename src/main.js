d3275b118f3cfaa1b0d8f260702704404f32765d
Jose Simões <jose.simoes@estudante.ufjf.br>
2023-12-03 14:56:33 -0300

    adiciona main.js

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { getMaria, getJoao, deJoaoParaMaria } from "./joaoEMaria.js";
+
+console. log("MAIN");
+setMaria(4);
+setJoao(2);
+
+
+let maria = getMaria();
+let joao = getJoao();
+console.log(maria, joao);
+
+deJoaoParaMaria();
+
+maria = getMaria();
+joao = getJoao();
+console.log(maria, joao);
